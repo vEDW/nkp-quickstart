2d41a7185095d18c9f6d17c6a7b6a929aa025686
Eric De Witte <eric@vedw.eu>
2024-12-08 17:51:42 +0100

    create airgap

--- scripts/nkp-create-mgmt-cluster.sh
+++ scripts/nkp-create-mgmt-cluster.sh
@@ -1,7 +1,7 @@
 source ./nkp-env
 
 nkp create cluster nutanix -c $CLUSTER_NAME \
-    --kind-cluster-image $REGISTRY_MIRROR_URL/mesosphere/konvoy-bootstrap:v$NKP_VERSION \
+    --kind-cluster-image mesosphere/konvoy-bootstrap:v$NKP_VERSION \
     --endpoint https://$NUTANIX_ENDPOINT:$NUTANIX_PORT \
     --insecure \
     --kubernetes-service-load-balancer-ip-range $LB_IP_RANGE \
