83315dfb4d64bf42b2136e56845f9cdea8cf0cd8
Eric De Witte <eric@vedw.eu>
2025-01-04 16:45:24 +0100

    v2.13

--- scripts/workload-clusters/nkp-create-workload-cluster.sh
+++ scripts/workload-clusters/nkp-create-workload-cluster.sh
@@ -8,14 +8,14 @@ nkp create cluster nutanix -c $CLUSTER_NAME \
     --control-plane-vm-image $NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME \
     --control-plane-prism-element-cluster $NUTANIX_PRISM_ELEMENT_CLUSTER_NAME \
     --control-plane-subnets $NUTANIX_SUBNET_NAME \
-    --control-plane-replicas 1 \
+    --control-plane-replicas 3 \
     --worker-vm-image $NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME \
     --worker-prism-element-cluster $NUTANIX_PRISM_ELEMENT_CLUSTER_NAME \
     --worker-subnets $NUTANIX_SUBNET_NAME \
-    --worker-replicas 3 \
+    --worker-replicas 4 \
     --csi-storage-container $NUTANIX_STORAGE_CONTAINER_NAME \
     --csi-hypervisor-attached-volumes=false \
-    --registry-mirror-url http://$REGISTRY_MIRROR_URL \
+    --registry-mirror-url $REGISTRY_MIRROR_URL \
     --registry-mirror-password=$REGISTRY_MIRROR_USERNAME \
     --registry-mirror-username="$REGISTRY_MIRROR_PASSWORD" \
     --dry-run -o yaml > $CLUSTER_NAME.yaml
