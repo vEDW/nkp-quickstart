ef612eeebca9daa9f58e47a9f643a4ec6e31e202
Eric De Witte <eric@vedw.eu>
2025-01-05 15:21:27 +0100

    add PE cluster

--- scripts/workload-clusters/nkp-create-nodepool.sh
+++ scripts/workload-clusters/nkp-create-nodepool.sh
@@ -1,8 +1,8 @@
 source ./cluster-env
 
 NODEPOOL_NAME=dh1
+NUTANIX_PRISM_ELEMENT_CLUSTER_NAME=dh1
 NUTANIX_SUBNET_NAME=User.dh1
-
 NAMESPACE=$(kubectl get clusters -o json -A |jq -r --arg CLUSTER $CLUSTER_NAME '.items[].metadata|select(.name==$CLUSTER)|.namespace') 
 
 nkp create nodepool nutanix $NODEPOOL_NAME -c $CLUSTER_NAME -n $NAMESPACE \
