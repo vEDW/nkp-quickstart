fe2a52583171408321f0054636d4655ca81f31e1
Eric De Witte <eric@vedw.eu>
2025-01-15 12:05:41 -0800

    get kubeconfig

---
+++ scripts/workload-clusters/get-and-merge-kubeconfig.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+
+CONTEXTS=$(kubectl config get-contexts --output=name)
+echo
+echo "Select management cluster or CTRL-C to quit"
+select CONTEXT in $CONTEXTS; do 
+    echo "you selected cluster context : ${CONTEXT}"
+    echo 
+    CLUSTERCTX="${CONTEXT}"
+    break
+done
+
+kubectl config use-context $CLUSTERCTX
+
+CLUSTERS=$(kubectl get cluster --no-headers -A |awk '{print $2}')
+echo
+echo "Select workload cluster to get kubeconfig or CTRL-C to quit"
+select CLUSTER in $CLUSTERS; do 
+    CLUSTERNS=$(kubectl get cluster --no-headers -A |grep ${CLUSTER} | awk '{print $1}')
+    echo "you selected cluster  : ${CLUSTER} in namespace : ${CLUSTERNS}"
+    echo 
+    break
+done
+
+CLUSTERKUBEYAML=$(nkp get kubeconfig -c ${CLUSTER} -n ${CLUSTERNS})
+if [ $? -ne 0 ]; then
+    echo "get kubeconfig failed. Exiting."
+    exit 1
+fi
+
+#clusters
+CLUSTER=$(yq e '.clusters[]' $CLUSTERKUBEYAML )
+if [ $? -ne 0 ]; then
+    echo "getting clusters failed failed. Exiting."
+    exit 1
+fi
+CLUSTERNAME=$(echo "$CLUSTER" | yq e '.name')
+if [ $? -ne 0 ]; then
+    echo "getting cluster name failed. Exiting."
+    exit 1
+fi
+
+#users
+USERS=$(yq e '.users[]' $CLUSTERKUBEYAML  )
+if [ $? -ne 0 ]; then
+    echo "getting users failed. Exiting."
+    exit 1
+fi
+USERNAME=$(echo "$USERS" | yq e '.name')
+if [ $? -ne 0 ]; then
+    echo "getting user name failed. Exiting."
+    exit 1
+fi
+
+#contexts
+CONTEXTS=$(yq e '.contexts[]' $CLUSTERKUBEYAML  )
+if [ $? -ne 0 ]; then
+    echo "getting contexts failed. Exiting."
+    exit 1
+fi
+CONTEXTNAME=$(echo "$CONTEXTS" | yq e '.name')
+if [ $? -ne 0 ]; then
+    echo "getting context name failed. Exiting."
+    exit 1
+fi
+
+#kubeconfig file
+KUBECONF=$(yq e ~/.kube/config)
+if [ $? -ne 0 ]; then
+    echo "reading kubeconfig file failed. Exiting."
+    exit 1
+fi
+
+#delete cluster if present
+KUBECONF=$(echo "$KUBECONF" |test="$CLUSTERNAME" yq e 'del(.clusters[]|select (.name == env(test)))')
+#Add cluster
+KUBECONF=$(echo "$KUBECONF" |test="$CLUSTER" yq '.clusters +=[env(test)]')
+
+#delete user if present
+KUBECONF=$(echo "$KUBECONF" |test="$USERNAME" yq e 'del(.users[]|select (.name == env(test)))')
+#Add user
+KUBECONF=$(echo "$KUBECONF" |test="$USERS" yq '.users +=[env(test)]')
+
+#delete context if present
+KUBECONF=$(echo "$KUBECONF" |test="$CONTEXTNAME" yq e 'del(.contexts[]|select (.name == env(test)))')
+#Add context
+KUBECONF=$(echo "$KUBECONF" |test="$CONTEXTS" yq '.contexts +=[env(test)]')
+
+echo "$KUBECONF" | yq e
+if [ $? -ne 0 ]; then
+    echo "generating kubeconfig file failed. Exiting."
+    exit 1
+fi
+
+echo "$KUBECONF" | yq e > ~/.kube/config
+
