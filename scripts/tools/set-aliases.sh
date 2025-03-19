b32c85a85cd668d7ffeca26e203d0d8a876aecca
Eric De Witte <eric@vedw.eu>
2025-03-19 10:56:34 +0100

    add tmux.conf

--- scripts/tools/set-aliases.sh
+++ scripts/tools/set-aliases.sh
@@ -7,9 +7,21 @@ addrc(){
     fi
 }
 
+addtmux(){
+    content=$1
+    if [ "$(cat ~/.tmux.conf | grep "$content")" == "" ]; then
+        echo "$content" >> ~/.tmux.conf
+    fi
+}
+
 addrc 'alias k=kubectl' 
 addrc 'alias tn="tmux -2 new -s"' 
 addrc 'alias tl="tmux ls"'
 addrc 'alias ta="tmux a -t"'
 addrc 'alias kx=kubectx'
 addrc 'alias kns=kubens'
+
+source ~/.bashrc
+
+addtmux 'set -g default-terminal "screen-256color"'
+addtmux 'set -g mouse on'
