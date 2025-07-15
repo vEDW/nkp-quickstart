d804617128bf9a5dd7c1bf4bbef45d3db9c55fe0
Eric De Witte <eric@vedw.eu>
2025-07-15 10:25:39 +0200

    test workload cluster

--- workload-clusters/nkp-create-workload-cluster.sh
+++ workload-clusters/nkp-create-workload-cluster.sh
@@ -1,6 +1,14 @@
+#!/bin/bash
+
+#check if cluster-env file exists
+if [ ! -f ./cluster-env ]; then
+    echo "cluster-env file not found. Please create it with the required variables by cloning cluster-env.example."
+    exit 1
+fi
 source ./cluster-env
 
 nkp create cluster nutanix -c $CLUSTER_NAME \
+    ${CLUSTER_HOSTNAME:+--cluster-hostname "$CLUSTER_HOSTNAME"} \
     --endpoint https://$NUTANIX_ENDPOINT:$NUTANIX_PORT \
     --insecure \
     --kubernetes-service-load-balancer-ip-range $LB_IP_RANGE \
@@ -15,9 +23,12 @@ nkp create cluster nutanix -c $CLUSTER_NAME \
     --worker-replicas 4 \
     --csi-storage-container $NUTANIX_STORAGE_CONTAINER_NAME \
     --csi-hypervisor-attached-volumes=true \
-    --registry-mirror-url https://$REGISTRY_MIRROR_URL \
-    --registry-mirror-username=$REGISTRY_MIRROR_USERNAME \
-    --registry-mirror-password="$REGISTRY_MIRROR_PASSWORD" \
+    ${REGISTRY_MIRROR_URL:+--registry-mirror-url https://"$REGISTRY_MIRROR_URL"} \
+    ${REGISTRY_MIRROR_USERNAME:+--registry-mirror-username "$REGISTRY_MIRROR_USERNAME"} \
+    ${REGISTRY_MIRROR_PASSWORD:+--registry-mirror-password "$REGISTRY_MIRROR_PASSWORD"} \
+    ${REGISTRY_URL:+--registry-url https://"$REGISTRY_URL"} \
+    ${REGISTRY_USERNAME:+--registry-username "$REGISTRY_USERNAME"} \
+    ${REGISTRY_PASSWORD:+--registry-password "$REGISTRY_PASSWORD"} \
     --dry-run -o yaml > $CLUSTER_NAME.yaml
 
 echo "Cluster definition created:  $CLUSTER_NAME.yaml"
