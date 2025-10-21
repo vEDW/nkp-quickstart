a5e26507a1c04a09a2527d17e182ce4a944a1fc0
Eric De Witte <eric@vedw.eu>
2025-10-21 15:47:30 +0000

    workload cluster env

--- workload-clusters/nkp-create-workload-cluster.sh
+++ workload-clusters/nkp-create-workload-cluster.sh
@@ -16,19 +16,22 @@ nkp create cluster nutanix -c $CLUSTER_NAME \
     --control-plane-vm-image $NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME \
     --control-plane-prism-element-cluster $NUTANIX_PRISM_ELEMENT_CLUSTER_NAME \
     --control-plane-subnets $NUTANIX_SUBNET_NAME \
-    --control-plane-replicas 3 \
+    --control-plane-replicas $CONTROL_PLANE_REPLICAS \
     --worker-vm-image $NUTANIX_MACHINE_TEMPLATE_IMAGE_NAME \
     --worker-prism-element-cluster $NUTANIX_PRISM_ELEMENT_CLUSTER_NAME \
     --worker-subnets $NUTANIX_SUBNET_NAME \
-    --worker-replicas 4 \
+    --worker-replicas $WORKER_NODES_REPLICAS \
     --csi-storage-container $NUTANIX_STORAGE_CONTAINER_NAME \
-    --csi-hypervisor-attached-volumes=true \
+    --csi-hypervisor-attached-volumes=$CSI_HYPERVISOR_ATTACHED \
+    ${SSH_PUBLIC_KEY_FILE:+--ssh-public-key-file "$SSH_PUBLIC_KEY_FILE"} \
     ${REGISTRY_MIRROR_URL:+--registry-mirror-url https://"$REGISTRY_MIRROR_URL"} \
     ${REGISTRY_MIRROR_USERNAME:+--registry-mirror-username "$REGISTRY_MIRROR_USERNAME"} \
     ${REGISTRY_MIRROR_PASSWORD:+--registry-mirror-password "$REGISTRY_MIRROR_PASSWORD"} \
     ${REGISTRY_URL:+--registry-url https://"$REGISTRY_URL"} \
     ${REGISTRY_USERNAME:+--registry-username "$REGISTRY_USERNAME"} \
     ${REGISTRY_PASSWORD:+--registry-password "$REGISTRY_PASSWORD"} \
+    ${CP_CATEGORIES:+--control-plane-pc-categories "$CP_CATEGORIES"} \
+    ${WORKER_CATEGORIES:+--worker-pc-categories "$WORKER_CATEGORIES"} \
     --dry-run -o yaml > $CLUSTER_NAME.yaml
 
 if [ $? -ne 0 ]; then
