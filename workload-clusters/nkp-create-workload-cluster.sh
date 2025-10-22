2c5288405cca2689ab18479a378d49caf7ab3b03
Eric De Witte <eric@vedw.eu>
2025-10-22 05:54:41 +0000

    avoid default namespace when creating workload cluster

--- workload-clusters/nkp-create-workload-cluster.sh
+++ workload-clusters/nkp-create-workload-cluster.sh
@@ -1,11 +1,80 @@
 #!/bin/bash
 
+#function
+randgen(){
+    cat /dev/urandom | tr -dc 'a-z0-9' | head -c 5; echo
+}
+
 #check if cluster-env file exists
 if [ ! -f ./cluster-env ]; then
     echo "cluster-env file not found. Please create it with the required variables by cloning cluster-env.example."
     exit 1
 fi
 source ./cluster-env
+
+#select NKP Management Cluster kubeconfig context
+CONTEXTS=$(kubectl config get-contexts --output=name)
+#count number of contexts
+NUM_CONTEXTS=$(echo "$CONTEXTS" | wc -l)
+if [ "$NUM_CONTEXTS" -eq 0 ]; then
+    echo "No kubeconfig contexts found. Please configure your kubeconfig."
+    exit 1
+elif [ "$NUM_CONTEXTS" -eq 1 ]; then
+    CLUSTERCTX="$CONTEXTS"
+    echo "Only one kubeconfig context found. Using context: $CLUSTERCTX"
+else
+    echo "Multiple kubeconfig contexts found."
+    echo
+    echo "Select management cluster or CTRL-C to quit"
+    select CONTEXT in $CONTEXTS; do 
+        echo "you selected cluster context : ${CONTEXT}"
+        echo 
+        CLUSTERCTX="${CONTEXT}"
+        break
+    done
+
+    kubectl config use-context $CLUSTERCTX
+fi
+
+#check if current context is set to the management cluster
+CAPICRD=$(kubectl  api-resources |grep cluster.x-k8s.io)
+if [[ -z "$CAPICRD" ]]; then
+    echo "The current kubeconfig context is not a NKP Management Cluster."
+    exit 1
+fi
+
+#check NKP edition
+LICENSECRD=$(kubectl get licenses -n kommander -o json |jq -r '.items[].status.dkpLevel')
+#check if license is empty
+if [[ -z "$LICENSECRD" ]]; then
+    echo "No license found. Please check if the license is installed."
+    LICENSECRD="No License found"
+    exit 1
+else
+    echo
+    echo "NKP Edition: $LICENSECRD"
+    echo
+    if [[ "$LICENSECRD" == "Ultimate" ]]; then
+        #select workspace in which to create the workload cluster
+        WORKSPACES=$(kubectl get workspaces -o jsonpath="{.items[*].metadata.name}")
+        echo
+        echo "Select workspace in which to create the workload cluster or CTRL-C to quit"
+        select WORKSPACE in $WORKSPACES; do 
+            echo "you selected workspace : ${WORKSPACE}"
+            echo 
+            break
+        done
+        #get workspace namespace
+        WORKSPACE_NAMESPACE=$(kubectl get workspace $WORKSPACE -o jsonpath="{.spec.namespaceName}")
+
+    # else
+    #     echo
+    #     echo "generating random namespace for workload cluster"
+    #     WORKSPACE="$CLUSTER_NAME-$(randgen)"
+    #     WORKSPACE_NAMESPACE="$WORKSPACE-$(randgen)"
+    fi
+fi
+
 
 nkp create cluster nutanix -c $CLUSTER_NAME \
     ${CLUSTER_HOSTNAME:+--cluster-hostname "$CLUSTER_HOSTNAME"} \
