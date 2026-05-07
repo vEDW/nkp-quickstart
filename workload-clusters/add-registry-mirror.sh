f97a8f93995894262865eeedda218f31809a9a99
Eric De Witte <eric@vedw.eu>
2026-05-07 17:50:19 +0200

    add echo

--- workload-clusters/add-registry-mirror.sh
+++ workload-clusters/add-registry-mirror.sh
@@ -121,6 +121,10 @@ if [[ "$REMOVE_REGISTRY_MIRROR_CONFIRMED" == true ]]; then
     CLUSTER_YAML=$(echo "$CLUSTER_YAML" | yq 'del(.spec.topology.variables[] | select(.value.globalImageRegistryMirror.url != null))')
 fi
 #add new registry mirror
+echo
+echo "Adding new registry mirror to cluster configuration..."
+echo "New registry mirror URL: $REGISTRYMIRROR"
+echo
 CLUSTER_YAML=$(echo "$CLUSTER_YAML" | yq '.spec.topology.variables[].value += {"globalImageRegistryMirror": {"url": "'$REGISTRYMIRROR'"}}')
 echo "$CLUSTER_YAML" > $CLUSTERNAME-updated.yaml
 echo "Updated cluster yaml with new registry mirror. to apply changes run :"
