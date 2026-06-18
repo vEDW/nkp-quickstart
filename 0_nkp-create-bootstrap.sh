988946dddfacc0e2a0dd6a62bb3d7ab9a311a528
Eric De Witte <eric@vedw.eu>
2026-06-18 14:23:35 +0200

    bootstrap

---
+++ 0_nkp-create-bootstrap.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+bundlepath=$(cat bundle-path)
+if [ $? -ne 0 ]; then
+    echo "no bundle-path file present."
+    exit 1
+fi
+
+# Check if directory is empty
+if [ -z "$bundlepath" ]; then
+    echo "No content in dir $bundlepath. Exiting."
+    exit 1
+fi
+
+echo "using airgap bundle: $bundlepath"
+
+source ./nkp-env
+
+NKP_VERSION=$($bundlepath/cli/nkp version -o=json |jq -r '.nkp.gitVersion')
+if [ $? -ne 0 ]; then
+    echo "nkp cli not found in $bundlepath/cli/nkp"
+    exit 1
+fi
+
+$bundlepath/cli/nkp create bootstrap
