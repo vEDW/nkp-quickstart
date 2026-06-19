0c13b9528316501e7ddca5afa5447f5d9fd120ab
Eric De Witte <eric@vedw.eu>
2026-06-19 08:04:01 +0200

    cluster yaml

---
+++ 2_nkp-create-mgmt-cluster-yaml.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+bundlepath=$(cat bundle-path)
+if [ $? -ne 0 ]; then
+    echo "no bundle-path file present."
+    exit 1
+fi
+
+# Check if directory is empty
+if [ -z "$bundlepath" ]; then
+    echo "No content in dir $bundlepath. Exiting."
+    exit 1
+fi
+
+echo "using airgap bundle: $bundlepath"
+
+source ./nkp-env
+
+NKP_VERSION=$($bundlepath/cli/nkp version -o=json |jq -r '.nkp.gitVersion')
+if [ $? -ne 0 ]; then
+    echo "nkp cli not found in $bundlepath/cli/nkp"
+    exit 1
+fi
+
+# Create the NKP cluster manifest
+$bundlepath/cli/nkp create cluster preprovisioned \
+  --cluster-name ${CLUSTER_NAME} \
+  --control-plane-endpoint-host ${CLUSTER_VIP} \
+  --virtual-ip-interface ${CLUSTER_VIP_ETH_INTERFACE} \
+  --pre-provisioned-inventory-file=preprovisioned_inventory.yaml \
+  --ssh-private-key-file=${SSH_PRIVATE_KEY_FILE} \
+  --registry-mirror-url=${REGISTRY_URL} \
+  --registry-mirror-username=${REGISTRY_USERNAME} \
+  --registry-mirror-password=${REGISTRY_PASSWORD} \
+  --registry-mirror-cacert=${REGISTRY_CA} \
+  --worker-replicas=4 \
+  --dry-run --output=yaml > ${CLUSTER_NAME}.yaml
+
+  echo "Cluster manifest for ${CLUSTER_NAME} created at ${CLUSTER_NAME}.yaml"
+  echo "You can apply this manifest to your management cluster with: kubectl apply -f ${CLUSTER_NAME}.yaml --server-side=true"
