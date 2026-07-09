c44b24b9929206773b8bc7094c055b59a52f053e
Eric De Witte <eric@vedw.eu>
2026-07-09 22:07:20 +0200

    disable selinux and get kubeconfig

--- workload-clusters/get-and-merge-kubeconfig.sh
+++ workload-clusters/get-and-merge-kubeconfig.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+#check if $KUBECONFIG empty
+if [ -z "$KUBECONFIG" ]; then
+    echo "KUBECONFIG environment variable is not set."
+    echo "checking if ~/.kube/config exists"
+    if [ ! -f ~/.kube/config ]; then
+        echo "kubeconfig file ~/.kube/config does not exist. Exiting."
+        exit 1
+    fi
+    KUBECONFIG=~/.kube/config
+fi
+
 if [ "$1" == "" ] ; then
 
     CONTEXTS=$(kubectl config get-contexts --output=name)
@@ -93,11 +104,16 @@ KUBECONF=$(echo "$KUBECONF" |test="$CONTEXTNAME" yq e 'del(.contexts[]|select (.
 #Add context
 KUBECONF=$(echo "$KUBECONF" |test="$CONTEXTS" yq '.contexts +=[env(test)]')
 
-echo "$KUBECONF" | yq e
+echo "$KUBECONF" | yq e 2>&1 /dev/null
 if [ $? -ne 0 ]; then
     echo "generating kubeconfig file failed. Exiting."
     exit 1
 fi
 
-echo "$KUBECONF" | yq e > ~/.kube/config
+echo "$KUBECONF" | yq e > ${KUBECONFIG}
+if [ $? -ne 0 ]; then
+    echo "writing kubeconfig file ${KUBECONFIG} failed. Exiting."
+    exit 1
+fi
 
+echo "kubeconfig file ${KUBECONFIG} updated with cluster : ${CLUSTERNAME} and context : ${CONTEXTNAME}"
